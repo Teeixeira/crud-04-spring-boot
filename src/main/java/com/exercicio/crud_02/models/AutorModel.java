5f9803af259053e06f0b3588482dcd890cc42759
Leo Teixeira <167496565+Teeixeira@users.noreply.github.com>
2026-03-18 19:02:39 -0300

    add: model

---
+++ src/main/java/com/exercicio/crud_02/models/AutorModel.java
@@ -0,0 +1,60 @@
+package com.exercicio.crud_02.models;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_autor")
+public class AutorModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "autor")
+    private String autor;
+    @Column(name = "nacionalidade")
+    private String nacionalidade;
+    private LocalDate dataNascimento;
+
+    public AutorModel() {
+    }
+
+    public AutorModel(Long id, String autor, String nacionalidade, LocalDate dataNascimento) {
+        this.id = id;
+        this.autor = autor;
+        this.nacionalidade = nacionalidade;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
